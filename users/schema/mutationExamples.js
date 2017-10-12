17b8804381c3dbe2605073fb0e2e79c6a4ad7b20
Reuben Greaves <reuben.greaves@wipro.com>
2017-10-12 12:10:32 +0100

    Initial commit

---
+++ users/schema/mutationExamples.js
@@ -0,0 +1,25 @@
+/*
+
+mutation {
+  addUser(firstName: "Stephen", age: 26) {
+    id,
+    firstName,
+    age
+  }
+}
+
+mutation {
+  deleteUser(id: "By8tbLfsZ") {
+    id
+  }
+}
+
+
+mutation {
+  editUser(id: "23", firstName: "Bob") {
+    id,
+    firstName
+  }
+}
+
+*/
---
+++ users/schema/queryExamples.js
@@ -0,0 +1,33 @@
+/*
+
+  Query examples
+
+
+  {
+    company(id: "2") {
+      id,
+      users {
+        id,
+        firstName
+      }
+    }
+  }
+
+  // Query Fragments
+
+  {
+    apple: company(id: "1") {
+      ...companyDetails
+    }
+    google: company(id: "2") {
+      ...companyDetails
+    }
+  }
+
+  fragment companyDetails on Company {
+    id,
+    name,
+    description
+  }
+  
+*/
